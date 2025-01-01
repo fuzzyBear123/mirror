4686c578412797741c83fd361f2a54e816686339
fuzzyBear123 <fuzzybear255@yahoo.com>
2025-01-01 10:44:31 +0000

    Change styles - updating docs

--- packages/schema-dts/test/graph.ts
+++ packages/schema-dts/test/graph.ts
@@ -2,28 +2,28 @@ import {Graph} from '../dist/schema';
 
 // "@context" and "@graph" are both required
 // @ts-expect-error Missing @graph and @context
-const _1: Graph = {};
+let _1: Graph = {};
 
 // @ts-expect-error Missing @context
-const _2: Graph = {'@graph': []};
+let _2: Graph = {'@graph': []};
 
 // @ts-expect-error Missing @graph
-const _3: Graph = {'@context': 'https://schema.org'};
+let _3: Graph = {'@context': 'https://schema.org'};
 
-const _4: Graph = {
+let _4: Graph = {
   '@context': 'https://schema.org',
   '@graph': [],
 };
 
 // "@context" must be correct.
-const _5: Graph = {
+let _5: Graph = {
   // @ts-expect-error Incorrect context
   '@context': 'https://google.com',
   '@graph': [],
 };
 
 // "@graph" can have full objects, and types
-const _6: Graph = {
+let _6: Graph = {
   '@context': 'https://schema.org',
   '@graph': [
     {'@type': 'Thing'},
@@ -33,7 +33,7 @@ const _6: Graph = {
 };
 
 // "@graph" still type-checks
-const _7: Graph = {
+let _7: Graph = {
   '@context': 'https://schema.org',
   '@graph': [
     {
