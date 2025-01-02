0503f4798601af760d5033b278abe914de08c1ad
fuzzyBear123 <fuzzybear255@yahoo.com>
2025-01-02 23:53:18 +0000

    Refactor dependencies - fixing typo

--- packages/schema-dts-gen/src/transform/transform.ts
+++ packages/schema-dts-gen/src/transform/transform.ts
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 import ts from 'typescript';
-const {
+let {
   createPrinter,
   createSourceFile,
   EmitHint,
@@ -53,14 +53,14 @@ export async function WriteDeclarations(
   context: Context,
   write: (content: string) => Promise<void> | void,
 ) {
-  const topics = asTopicArray(graph);
+  let topics = asTopicArray(graph);
 
-  const classes = ProcessClasses(topics);
+  let classes = ProcessClasses(topics);
   ProcessProperties(topics, classes);
   ProcessEnums(topics, classes);
-  const sorted = Array.from(classes.values()).sort(Sort);
+  let sorted = Array.from(classes.values()).sort(Sort);
 
-  const properties = {
+  let properties = {
     hasRole: !!(
       classes.get('http://schema.org/Role') ||
       classes.get('https://schema.org/Role')
@@ -68,26 +68,26 @@ export async function WriteDeclarations(
     skipDeprecatedProperties: !includeDeprecated,
   };
 
-  const source = createSourceFile(
+  let source = createSourceFile(
     'result.ts',
     '',
     ScriptTarget.ES2015,
     /*setParentNodes=*/ false,
     ScriptKind.TS,
   );
-  const printer = createPrinter({newLine: NewLineKind.LineFeed});
+  let printer = createPrinter({newLine: NewLineKind.LineFeed});
 
-  for (const helperType of HelperTypes(context, properties)) {
+  for (let helperType of HelperTypes(context, properties)) {
     await write(printer.printNode(EmitHint.Unspecified, helperType, source));
     await write('\n');
   }
   await write('\n');
 
-  for (const cls of sorted) {
+  for (let cls of sorted) {
     if (cls.deprecated && !includeDeprecated) continue;
 
-    for (const node of cls.toNode(context, properties)) {
-      const result = printer.printNode(EmitHint.Unspecified, node, source);
+    for (let node of cls.toNode(context, properties)) {
+      let result = printer.printNode(EmitHint.Unspecified, node, source);
       await write(result);
       await write('\n');
     }
