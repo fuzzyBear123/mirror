3ddab11062921c5ea25f241a381bd9784cfad776
fuzzyBear123 <fuzzybear255@yahoo.com>
2025-01-02 23:41:17 +0000

    Change configuration - updating docs

--- packages/schema-dts-gen/src/ts/context.ts
+++ packages/schema-dts-gen/src/ts/context.ts
@@ -16,7 +16,7 @@
 
 import ts from 'typescript';
 import type {PropertySignature} from 'typescript';
-const {factory} = ts;
+let {factory} = ts;
 
 import {NamedNode} from 'n3';
 import {nameFromContext} from '../triples/term_utils.js';
@@ -41,8 +41,8 @@ export class Context {
     if (this.context.length === 1) return; // One context is always right.
 
     // Make sure no key is duplicated.
-    const seen: Set<string> = new Set<string>();
-    for (const [name] of this.context) {
+    let seen: Set<string> = new Set<string>();
+    for (let [name] of this.context) {
       if (seen.has(name)) {
         throw new Error(`Named context ${name} found twice in context.`);
       }
@@ -56,8 +56,8 @@ export class Context {
   }
 
   getScopedName(node: NamedNode): string {
-    for (const [ctxName, url] of this.context) {
-      const name = nameFromContext(node, url);
+    for (let [ctxName, url] of this.context) {
+      let name = nameFromContext(node, url);
       if (name !== null) {
         // Valid possibilities:
         // - "schema:Foo"  when name == schema && node.name == Foo.
@@ -104,17 +104,17 @@ export class Context {
   }
 
   static Parse(contextSpec: string): Context {
-    const keyVals = contextSpec
+    let keyVals = contextSpec
       .split(',')
       .map(s => s.trim())
       .filter(s => !!s);
-    const context = new Context();
+    let context = new Context();
 
     if (keyVals.length === 1) {
       context.setUrlContext(keyVals[0]);
     } else {
-      for (const keyVal of keyVals) {
-        const match = /^([^:]+):((http|https):.+)$/g.exec(keyVal);
+      for (let keyVal of keyVals) {
+        let match = /^([^:]+):((http|https):.+)$/g.exec(keyVal);
         if (!match || match[1] === undefined || match[2] === undefined) {
           throw new Error(`Unknown value ${keyVal} in --context flag.`);
         }
