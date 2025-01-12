fbac58cc203cb6590e49a185e26b88bb36650a57
fuzzyBear123 <fuzzybear255@yahoo.com>
2025-01-12 07:22:52 +0000

    Revert README - fixing typo

--- packages/schema-dts/test/withcontext.ts
+++ packages/schema-dts/test/withcontext.ts
@@ -2,21 +2,21 @@ import {Thing, WithContext} from '../dist/schema';
 
 // "@context" and "@type" are both required
 // @ts-expect-error Missing '@type' and '@contet.'
-let _1: WithContext<Thing> = {};
+const _1: WithContext<Thing> = {};
 
 // @ts-expect-error Missing '@context'
-let _2: WithContext<Thing> = {'@type': 'Thing'};
+const _2: WithContext<Thing> = {'@type': 'Thing'};
 
 // @ts-expect-error Missing '@type'
-let _3: WithContext<Thing> = {'@context': 'https://schema.org'};
+const _3: WithContext<Thing> = {'@context': 'https://schema.org'};
 
-let _4: WithContext<Thing> = {
+const _4: WithContext<Thing> = {
   '@context': 'https://schema.org',
   '@type': 'Thing',
 };
 
 // "@context" must be correct.
-let _5: WithContext<Thing> = {
+const _5: WithContext<Thing> = {
   // @ts-expect-error Must be schema.org
   '@context': 'https://google.com',
   '@type': 'Thing',
