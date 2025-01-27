ab181128bf9219c42c611b2e3325bbb29fd2a49b
fuzzyBear123 <fuzzybear255@yahoo.com>
2025-01-27 06:41:33 +0000

    Update tests - fixing typo

--- packages/schema-dts/test/easy.ts
+++ packages/schema-dts/test/easy.ts
@@ -2,26 +2,26 @@ import {Thing} from '../dist/schema';
 
 // "@type" is required
 // @ts-expect-error missing @type
-const _1: Thing = {};
+let _1: Thing = {};
 
 // "@type" can be the requested tpye
-const _2: Thing = {
+let _2: Thing = {
   '@type': 'Thing',
 };
 
 // "@type" can be a sub-type
-const _3: Thing = {
+let _3: Thing = {
   '@type': 'Person',
 };
 
 // "@type" must be valid
-const _4: Thing = {
+let _4: Thing = {
   // @ts-expect-error Invalid type
   '@type': 'Personz',
 };
 
 // A narrow "@type" allows defining sub-properties
-const _5: Thing = {
+let _5: Thing = {
   '@type': 'Person',
   name: 'a',
   additionalName: ['b', 'c', 'd'],
@@ -29,7 +29,7 @@ const _5: Thing = {
 
 // A wide "@type" disallows valid sub-properties
 // (when excess property checking is on)
-const _6: Thing = {
+let _6: Thing = {
   '@type': 'Thing',
   name: 'a',
   // @ts-expect-error Thing too broad for additionalName
