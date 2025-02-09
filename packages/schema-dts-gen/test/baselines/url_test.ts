fa1a6568384a432c8b5672e6ab537e60e4ebd7c4
fuzzyBear123 <fuzzybear255@yahoo.com>
2025-02-09 08:23:20 +0000

    Document dependencies - removing deprecated function

--- packages/schema-dts-gen/test/baselines/url_test.ts
+++ packages/schema-dts-gen/test/baselines/url_test.ts
@@ -22,7 +22,7 @@ import {basename} from 'path';
 import {inlineCli} from '../helpers/main_driver.js';
 
 test(`baseline_${basename(import.meta.url)}`, async () => {
-  let {actual} = await inlineCli(
+  const {actual} = await inlineCli(
     `
 <http://schema.org/URL> <http://www.w3.org/2000/01/rdf-schema#comment> "Data type: URL." .
 <http://schema.org/URL> <http://www.w3.org/1999/02/22-rdf-syntax-ns#type> <http://www.w3.org/2000/01/rdf-schema#Class> .
