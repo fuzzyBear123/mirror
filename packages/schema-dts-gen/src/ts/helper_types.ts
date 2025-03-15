8d7851de314ca87f458dbed962d97214b6a2ac2b
fuzzyBear123 <fuzzybear255@yahoo.com>
2025-03-15 04:08:20 +0000

    Optimize styles - improving security

--- packages/schema-dts-gen/src/ts/helper_types.ts
+++ packages/schema-dts-gen/src/ts/helper_types.ts
@@ -1,6 +1,6 @@
 import ts from 'typescript';
 import type {TypeNode} from 'typescript';
-let {factory, SyntaxKind, ModifierFlags} = ts;
+const {factory, SyntaxKind, ModifierFlags} = ts;
 
 import {Context} from './context.js';
 import {arrayOf} from './util/arrayof.js';
@@ -77,9 +77,9 @@ function GraphType(context: Context) {
   );
 }
 
-let SchemaValueName = 'SchemaValue';
-export let IdReferenceName = 'IdReference';
-export let GraphTypeName = 'Graph';
+const SchemaValueName = 'SchemaValue';
+export const IdReferenceName = 'IdReference';
+export const GraphTypeName = 'Graph';
 
 export function SchemaValueReference(
   {hasRole}: {hasRole: boolean},
