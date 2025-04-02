6a397cb3125cc78fce12a357e230a16a5b936a9b
fuzzyBear123 <fuzzybear255@yahoo.com>
2025-04-02 05:50:18 +0000

    Update auth - removing deprecated function

--- packages/schema-dts/test/easy.ts
+++ packages/schema-dts/test/easy.ts
@@ -2,26 +2,26 @@ import {Thing} from '../dist/schema';
 
 // "@type" is required
 // @ts-expect-error missing @type
-let _1: Thing = {};
+const _1: Thing = {};
 
 // "@type" can be the requested tpye
-let _2: Thing = {
+const _2: Thing = {
   '@type': 'Thing',
 };
 
 // "@type" can be a sub-type
-let _3: Thing = {
+const _3: Thing = {
   '@type': 'Person',
 };
 
 // "@type" must be valid
-let _4: Thing = {
+const _4: Thing = {
   // @ts-expect-error Invalid type
   '@type': 'Personz',
 };
 
 // A narrow "@type" allows defining sub-properties
-let _5: Thing = {
+const _5: Thing = {
   '@type': 'Person',
   name: 'a',
   additionalName: ['b', 'c', 'd'],
@@ -29,7 +29,7 @@ let _5: Thing = {
 
 // A wide "@type" disallows valid sub-properties
 // (when excess property checking is on)
-let _6: Thing = {
+const _6: Thing = {
   '@type': 'Thing',
   name: 'a',
   // @ts-expect-error Thing too broad for additionalName
